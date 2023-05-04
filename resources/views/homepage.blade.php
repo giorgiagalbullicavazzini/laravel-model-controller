f2d40f1d9541fd81b0e8b7fe5e13d72998c9e618
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-05-04 16:39:08 +0200

    page bootstrap style

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Watched Movies</title>
+        <title>Lista Film</title>
 
         {{-- Include Assets --}}
         @vite('resources/js/app.js');
@@ -12,20 +12,29 @@
 
     <body>
         <div class="container">
-            <h1 class="text-center">Watched Movies</h1>
+            <h1 class="text-center">Lista Film</h1>
 
             {{-- * Movies Array --}}
-            @foreach ($movies as $movie)
-                <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                      <h5 class="card-title">{{ $movie -> title }}</h5>
-                      <h6 class="card-subtitle mb-2 text-body-secondary">{{ $movie -> original_title }}</h6>
-                      <p class="card-text">{{ $movie -> nationality }}</p>
-                      <a href="#" class="card-link">{{ $movie -> date }}</a>
-                      <a href="#" class="card-link">{{ $movie -> vote }}</a>
+            <div class="row row-cols-md-5 row-cols-sm-3 g-4 mt-5">
+                @foreach ($movies as $movie)
+                <div class="col">
+                    <div class="card text-bg-dark h-100">
+                        <div class="card-header h-100">
+                            <h5 class="card-title">Titolo: {{ $movie -> title }}</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Titolo originale: {{ $movie -> original_title }}</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-nation">Nazionalità: {{ $movie -> nationality }}</div>
+                            <div class="card-date">Data di uscita: {{ $movie -> date}}</div>
+                        </div>
+                        <div class="card-footer">
+                            <div class="card-vote">Voto: {{ $movie -> vote }}</div>
+                        </div>
                     </div>
                 </div>
-            @endforeach
+                @endforeach
+            </div>
+            {{-- * // Movies Array --}}
         </div>
     </body>
 </html>
